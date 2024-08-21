679b30e709fd91e8b0d2cca349cd3fd752764b8b
Sofya_Voronova <savoronova@edu.hse.ru>
2024-08-21 22:01:45 +0300

    Card

--- pokemonProject/src/App.tsx
+++ pokemonProject/src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card } from 'antd';
 import FormSignIn from './components/SignForm';
-import FormSignUp from './components/SignFormUp'
-import { Tabs } from 'antd';
-import type { TabsProps } from 'antd';
+import FormSignUp from './components/SignFormUp';
 
-const onChange = (key: string) => {
-  console.log(key);
-};
-
-const items: TabsProps['items'] = [
+const tabListNoTitle = [
   {
-    key: '1',
+    key: 'signup',
     label: 'Sign up',
-    children: <FormSignUp/>,
   },
   {
-    key: '2',
+    key: 'signin',
     label: 'Sign in',
-    children: <FormSignIn/>,
   }
 ];
 
-const App: React.FC = () => <Tabs defaultActiveKey="1" items={items} onChange={onChange} />;
+const contentListNoTitle: Record<string, React.ReactNode> = {
+  signup: <p><FormSignUp/></p>,
+  signin: <p><FormSignIn/></p>,
+};
+
+const App: React.FC = () => {
+  const [activeTabKey2, setActiveTabKey2] = useState<string>('app');
+
+  const onTab2Change = (key: string) => {
+    setActiveTabKey2(key);
+  };
+
+  return (
+    <>
+      <Card
+        style={{ width: '100%' }}
+        tabList={tabListNoTitle}
+        activeTabKey={activeTabKey2}
+        onTabChange={onTab2Change}
+        tabProps={{
+          size: 'middle',
+        }}
+      >
+        {contentListNoTitle[activeTabKey2]}
+      </Card>
+    </>
+  );
+};
+
 export default App;
+
+
+
+// import React from 'react';
+// import FormSignIn from './components/SignForm';
+// import FormSignUp from './components/SignFormUp';
+// import { Tabs } from 'antd';
+// import type { TabsProps } from 'antd';
+
+// const onChange = (key: string) => {
+//   console.log(key);
+// };
+
+// const items: TabsProps['items'] = [
+//   {
+//     key: '1',
+//     label: 'Sign up',
+//     children: <FormSignUp/>,
+//   },
+//   {
+//     key: '2',
+//     label: 'Sign in',
+//     children: <FormSignIn/>,
+//   }
+// ];
+
+// const App: React.FC = () => <Tabs defaultActiveKey="1" items={items} onChange={onChange} />;
+// export default App;
 
