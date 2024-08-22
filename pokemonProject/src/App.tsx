4d6ac1df9841a3bb8aec9b7768a46ae80865c907
Sofya_Voronova <savoronova@edu.hse.ru>
2024-08-22 14:31:56 +0300

    Form done

--- pokemonProject/src/App.tsx
+++ pokemonProject/src/App.tsx
@@ -3,34 +3,40 @@ import { Card, Flex } from 'antd';
 import FormSignIn from './components/SignForm';
 import FormSignUp from './components/SignFormUp';
 
+enum AuthTab {
+  SignIn = 'SignIn',
+  SignUp = 'SignUp',
+}
+
 const tabListNoTitle = [
   {
-    key: 'signup',
+    key: AuthTab.SignUp,
     label: 'Sign up',
   },
   {
-    key: 'signin',
+    key: AuthTab.SignIn,
     label: 'Sign in',
   }
 ];
 
-const contentListNoTitle: Record<string, React.ReactNode> = {
-  signup: <p><FormSignUp/></p>,
-  signin: <p><FormSignIn/></p>,
+
+const contentListNoTitle: Record<AuthTab, React.ReactNode> = {
+  [AuthTab.SignUp]: <FormSignUp/>,
+  [AuthTab.SignIn]: <FormSignIn/>,
 };
 
 const App: React.FC = () => {
-  const [activeTabKey2, setActiveTabKey2] = useState<string>('app');
+  const [activeTabKey2, setActiveTabKey2] = useState<AuthTab>(AuthTab.SignUp);
 
   const onTab2Change = (key: string) => {
-    setActiveTabKey2(key);
+    setActiveTabKey2(key as AuthTab);
   };
 
   return (
-    <Flex gap="middle" align='start' vertical justify='center'>
+    <Flex align='center'  justify='center' style={{height:'100vh', width:'100vw', placeItems: 'center',}} >
 
       <Card
-        style={{ width: '400px',}}
+        style={{ width: '400px', display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center'}}
         tabList={tabListNoTitle}
         activeTabKey={activeTabKey2}
         onTabChange={onTab2Change}
