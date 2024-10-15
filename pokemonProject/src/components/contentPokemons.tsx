b4fc09ca7aee7732329ff844d30335fd3e782cd8
Sofya_Voronova <savoronova@edu.hse.ru>
2024-10-15 11:16:39 +0300

    Content and Siders

---
+++ pokemonProject/src/components/contentPokemons.tsx
@@ -0,0 +1,58 @@
+import type { CSSProperties } from 'react';
+import React from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import type { CollapseProps } from 'antd';
+import { Collapse, theme } from 'antd';
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const getItems: (panelStyle: CSSProperties) => CollapseProps['items'] = (panelStyle) => [
+  {
+    key: '1',
+    label: <span className='YellowTextStyle' style={{marginLeft:'20px'}}>My Pokemons</span>,
+    children: <p>{text}</p>,
+    style: panelStyle,
+  },
+  {
+    key: '2',
+    label: <span className='YellowTextStyle' style={{marginLeft:'20px'}}>Garden</span>,
+    children: <p>{text}</p>,
+    style: panelStyle,
+  },
+  {
+    key: '3',
+    label: <span className='YellowTextStyle' style={{marginLeft:'20px', }}>Hunt</span>,
+    children: <p>{text}</p>,
+    style: panelStyle,
+  },
+];
+
+const ContentPokemons: React.FC = () => {
+  const { token } = theme.useToken();
+
+  const panelStyle: React.CSSProperties = {
+    marginBottom: 24,
+    borderRadius: token.borderRadiusLG,
+    border: 'none',
+    backgroundColor:'#ffff',
+    boxShadow: '0px 0px 16px 0px rgba(58, 58, 58, 0.1)',
+    
+  };
+
+  return (
+    <Collapse
+      bordered={false}
+      defaultActiveKey={['1']}
+      expandIcon={({ isActive }) => <DownOutlined rotate={isActive ? 180 : 0} style={{color:' rgba(54, 95, 172, 1)', width:'16px', height:'10px', marginTop:'17px'}}/>}
+      expandIconPosition='end'
+      style={{ background: '#0000' }}
+      items={getItems(panelStyle)}
+    />
+  );
+};
+
+export default ContentPokemons;
--- pokemonProject/src/components/pokemonPage.tsx
+++ pokemonProject/src/components/pokemonPage.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Flex, Layout } from 'antd';
 import './pokemon.css'
+import ContentPokemons from './contentPokemons';
+
 const { Header, Sider, Content } = Layout;
 
 const PokemonPage: React.FC = () => (
   <Flex gap="middle" wrap style={{ width: '100vw', height: '100vh' }}>
-
     <Layout className="layoutStyle">
-      <Header className="headerStyle">Header</Header>
+      <Header className="headerStyle">
+        <div className='headerLogoStyle'>
+            <img src="../img/pokemon.png" alt="pokemon" style={{height:'54px', width:'149px', marginRight:'5px'}}/>
+            <hr style={{color: 'rgba(239, 239, 239, 1)', height:'54px', width:'2px', border:'1px solid rgba(239, 239, 239, 1)' }}/>
+            <img src="../img/clicker.png" alt="pokemon" style={{height:'54px', width:'149px', }}/>
+        </div>
+        <div className='headerLogoStyle'>
+            <img src="../img/coin.png" alt="coin" style={{height:'32px', width:'32px', marginRight:'5px', justifyContent:'flex-end'}}/>
+            <span className='YellowTextStyle'>100000000</span> 
+        </div>
+      </Header>
       <Layout>
-        <Sider width="25%" className="siderStyle">
-          Sider
+        <Sider width="25%" className="siderStyle" style={{backgroundColor:'#ffff'}}>
+        <span className='YellowTextStyle' style={{marginLeft:'20px', }}>Inventory</span> 
+
         </Sider>
-        <Content className="contentStyle">Content</Content>
-        <Sider width="25%" className="siderStyle">
-          Sider
+        <Content className="contentStyle" >
+        <ContentPokemons/>
+        </Content>
+        <Sider width="25%" className="siderStyle" style={{backgroundColor:'#ffff'}}>
+        <span className='YellowTextStyle' style={{marginLeft:'20px'}}>Shop</span> 
         </Sider>
       </Layout>
     </Layout>
