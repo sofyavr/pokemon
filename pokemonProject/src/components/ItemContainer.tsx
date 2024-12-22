7a5acde17fa9bfa1602e7ad93f7d1e9af9c0318d
Sofya_Voronova <savoronova@edu.hse.ru>
2024-12-22 18:06:17 +0300

    Вывод айтемов

--- pokemonProject/src/components/ItemContainer.tsx
+++ pokemonProject/src/components/ItemContainer.tsx
@@ -1,84 +1,146 @@
+// import { itemApi } from "../services/allItems";
+// // import { IArray } from "../models/IArray";
+// import {ItemItem} from "./ItemItem";
+// import {Card} from 'antd'
+// import './pokemon.css'
+
+// const ItemContainer = () => {
+//     // const { data: response } = itemApi.useGetAllItemsQuery('');
+//     // console.log(response);
+//     const { data: greatball } = itemApi.useGetGreatBallQuery('');
+//     const { data: duskball } = itemApi.useGetDuskBallQuery('');
+//     const { data: netball } = itemApi.useGetNetBallQuery('');
+//     const { data: antidote } = itemApi.useGetAntidoteQuery('');
+//     const { data: iceheal } = itemApi.useGetIceHealQuery('');
+//     const {data: itemid} = itemApi.useGetItemByNameQuery('');
+//     return (
+//         <div>
+//             <div>
+//                 {/* {
+//                     response && response.results.map((item: IArray) =>
+//                         <ItemItem key={item.name} item={item} />
+//                     )
+//                 } */}
+//                 {
+//                     itemid && (
+//                         <ItemItem key={itemid.results.name} name={itemid.results.name}  pollingInterval={0}/>
+//                     )
+//                 }
+//                                 {
+//                     itemid && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {itemid.sprites && itemid.sprites.default && (
+//                                     <img src={itemid.sprites.default} alt={itemid.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{itemid.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }
+//                 {
+//                     greatball && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {greatball.sprites && greatball.sprites.default && (
+//                                     <img src={greatball.sprites.default} alt={greatball.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{greatball.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }
+//                                 {
+//                     duskball && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {duskball.sprites && duskball.sprites.default && (
+//                                     <img src={duskball.sprites.default} alt={duskball.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{duskball.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }                {
+//                     netball && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {netball.sprites && netball.sprites.default && (
+//                                     <img src={netball.sprites.default} alt={netball.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{netball.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }                {
+//                     antidote && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {antidote.sprites && antidote.sprites.default && (
+//                                     <img src={antidote.sprites.default} alt={antidote.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{antidote.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }                {
+//                     iceheal && (
+//                         <Card className="itemCard">
+//                             <div className="cardContent">
+//                                 {iceheal.sprites && iceheal.sprites.default && (
+//                                     <img src={iceheal.sprites.default} alt={iceheal.name} className="photo"/>
+//                                 )}
+//                                 <b className="title">{iceheal.name}  </b>
+//                             </div>
+//                             <button className="button">Кнопка</button>
+//                         </Card>
+//                     )
+//                 }
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ItemContainer;
 import { itemApi } from "../services/allItems";
-// import { IArray } from "../models/IArray";
-// import ItemItem from "./ItemItem";
-import {Card} from 'antd'
-import './pokemon.css'
+import { ItemItem } from "./ItemItem";
+import { Card } from 'antd';
+import './pokemon.css';
 
 const ItemContainer = () => {
-    // const { data: response } = itemApi.useGetAllItemsQuery('');
-    // console.log(response);
-    const { data: greatball } = itemApi.useGetGreatBallQuery('');
-    // console.log(greatball);
-    const { data: duskball } = itemApi.useGetDuskBallQuery('');
-    const { data: netball } = itemApi.useGetNetBallQuery('');
-    const { data: antidote } = itemApi.useGetAntidoteQuery('');
-    const { data: iceheal } = itemApi.useGetIceHealQuery('');
+    // Получаем список всех айтемов
+    const { data: response } = itemApi.useGetAllItemsQuery('');
+    
+    // Проверяем, есть ли данные и есть ли хотя бы один айтем в списке
+    const itemName = response && response.results.length > 0 ? response.results[0].name : '';
+
+    // Получаем данные конкретного айтема по имени
+    const { data: itemid } = itemApi.useGetItemByNameQuery(itemName);
 
     return (
         <div>
             <div>
-                {/* {
-                    response && response.results.map((item: IArray) =>
-                        <ItemItem key={item.name} item={item} />
-                    )
-                } */}
+                {/* Отображаем список айтемов, если они есть */}
                 {
-                    greatball && (
-                        <Card className="itemCard">
-                            <div className="cardContent">
-                                {greatball.sprites && greatball.sprites.default && (
-                                    <img src={greatball.sprites.default} alt={greatball.name} className="photo"/>
-                                )}
-                                <b className="title">{greatball.name}  </b>
-                            </div>
-                            <button className="button">Кнопка</button>
-                        </Card>
-                    )
+                    response && response.results.map((item) => (
+                        <ItemItem key={item.name} name={item.name} pollingInterval={0}/>
+                    ))
                 }
-                                {
-                    duskball && (
+
+                {/* Отображаем конкретный айтем, если он загружен */}
+                {
+                    itemid && (
                         <Card className="itemCard">
                             <div className="cardContent">
-                                {duskball.sprites && duskball.sprites.default && (
-                                    <img src={duskball.sprites.default} alt={duskball.name} className="photo"/>
+                                {itemid.sprites && itemid.sprites.default && (
+                                    <img src={itemid.sprites.default} alt={itemid.name} className="photo" />
                                 )}
-                                <b className="title">{duskball.name}  </b>
-                            </div>
-                            <button className="button">Кнопка</button>
-                        </Card>
-                    )
-                }                {
-                    netball && (
-                        <Card className="itemCard">
-                            <div className="cardContent">
-                                {netball.sprites && netball.sprites.default && (
-                                    <img src={netball.sprites.default} alt={netball.name} className="photo"/>
-                                )}
-                                <b className="title">{netball.name}  </b>
-                            </div>
-                            <button className="button">Кнопка</button>
-                        </Card>
-                    )
-                }                {
-                    antidote && (
-                        <Card className="itemCard">
-                            <div className="cardContent">
-                                {antidote.sprites && antidote.sprites.default && (
-                                    <img src={antidote.sprites.default} alt={antidote.name} className="photo"/>
-                                )}
-                                <b className="title">{antidote.name}  </b>
-                            </div>
-                            <button className="button">Кнопка</button>
-                        </Card>
-                    )
-                }                {
-                    iceheal && (
-                        <Card className="itemCard">
-                            <div className="cardContent">
-                                {iceheal.sprites && iceheal.sprites.default && (
-                                    <img src={iceheal.sprites.default} alt={iceheal.name} className="photo"/>
-                                )}
-                                <b className="title">{iceheal.name}  </b>
+                                <b className="title">{itemid.name}</b>
                             </div>
                             <button className="button">Кнопка</button>
                         </Card>
--- pokemonProject/src/components/ItemItem.tsx
+++ pokemonProject/src/components/ItemItem.tsx
@@ -1,17 +1,54 @@
-import  { FC } from "react";
-import { IArray } from "../models/IArray";
+// import  { FC } from "react";
+// import { IArray } from "../models/IArray";
 
-interface ItemItemProps{
-    key: string;
-    item: IArray;
+// interface ItemItemProps{
+//     key: string;
+//     item: IArray;
+// }
+
+// const ItemItem: FC<ItemItemProps> = ({item}) => {
+//     return (
+//         <div style={{color:"black"}}>
+//         {item.name}
+//         </div>
+//     );
+// };
+
+// export default ItemItem;
+import { useGetItemByNameQuery } from '../services/allItems'
+import { Card, Space, } from 'antd'
+import { SettingFilled } from '@ant-design/icons';
+import './pokemon.css'
+
+export const ItemItem = ({
+  name,
+  pollingInterval,
+}: {
+  name: string
+  pollingInterval: number
+}) => {
+  const { data, error, isLoading, } = useGetItemByNameQuery(
+    name,
+    {
+      pollingInterval,
+    },
+  )
+
+  return (
+    <>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <Space>
+            <Card className="pokemonCard">
+                <b style={{display:'flex', justifyContent:'space-between'}}>{data.name} <SettingFilled style={{stroke:'rgba(54, 95, 172, 1)', color:'rgba(255, 204, 1, 1)', strokeWidth:'70px'}}/> </b>
+                <img src={data.sprites.default} alt={data.name} style={{width:'100%', marginTop:'15px'}}/>
+            </Card>
+         </Space>
+        
+      ) : null}
+    </>
+  )
 }
-
-const ItemItem: FC<ItemItemProps> = ({item}) => {
-    return (
-        <div style={{color:"black"}}>
-        {item.name}
-        </div>
-    );
-};
-
-export default ItemItem;
